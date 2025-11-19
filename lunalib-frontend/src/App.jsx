484c992af5c938b06555dc50a39e7c2ac721a706
nguad774 <nidiag0618@gmail.com>
2025-11-19 16:53:56 -0600

    enabling BrowserRouter for multipage app

--- lunalib-frontend/src/App.jsx
+++ lunalib-frontend/src/App.jsx
@@ -1,4 +1,11 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Home from './pages/Home.jsx'
+import MyLibrary from './pages/MyLibrary.jsx' 
+import BookDetails from './pages/BookDetails.jsx'
+import Auth from './pages/Auth.jsx'
+import Settings from './pages/Settings.jsx'
+
 
 // Import images from src/assets
 import Logo from './assets/logo.png';
@@ -11,8 +18,15 @@ import SettingsIcon from './assets/settings.png';
 function App() {
 
   return (
-    <>
-    </>
+   <BrowserRouter> 
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/library' element={<MyLibrary />} />
+        <Route path='/book/:id' element={<BookDetails />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/settings' element={<Settings />} />
+      </Routes>
+   </BrowserRouter>
   );
 }
 
